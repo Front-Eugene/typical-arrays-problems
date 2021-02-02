b83d8d0b0bc4fef771d47167b1d7f0d94314a826
Eugene Morozenko <72557729+Front-Eugene@users.noreply.github.com>
2021-02-02 15:51:55 +0300

    My solution.
    My solution.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arrNum) {
+  let minElement = Math.min.apply(null, arrNum);
+  if (arrNum === undefined || arrNum.length === 0) {
+    return 0
+  }
+  return minElement;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arrNum) {
+  let maxElement = Math.max.apply(null, arrNum);
+  if (arrNum === undefined || arrNum.length === 0) {
+    return 0
+  }
+  return maxElement;
+} 
+  
+exports.avg = function avg (arrNum) {
+  if (arrNum === undefined || arrNum.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < arrNum.length; i++) { 
+  sum += arrNum[i];
+  }
+  return sum / arrNum.length;
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
